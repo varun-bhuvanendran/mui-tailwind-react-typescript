2a2917fc4f1a86b4a61976267a95100e28a4b105
Varun Bhuvanendran <varun.bhuvanendran@thameswater.co.uk>
2022-08-02 17:14:07 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from "@mui/material/Button"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="grid grid-cols-2 justify-items-center">
+        <h2>MUI</h2>
+
+        <h2>MUI + Tailwind</h2>
+      </div>
+
+      <div className="grid grid-cols-2 justify-items-center gap-6">
+        {/* MUI */}
+        <div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              alert("clicked mui button")
+            }}
+          >
+            Clickable + Contained
+          </Button>
+          <div>
+            <code>
+              <code>
+                &lt;Button variant="contained"&gt; Clickable + Contained &lt;/Button&gt;
+              </code>
+            </code>
+          </div>
+        </div>
+        {/* MUI + Tailwind */}
+        <div>
+          <Button
+            variant="contained"
+            onClick={() => {
+              alert("clicked mui button styled with tailwind!")
+            }}
+            className="bg-red-700 text-white w-full hover:bg-red-300"
+          >
+            Clickable + Contained + Red + full width!
+          </Button>
+          <div>
+            <code>
+              &lt;Button variant="contained" className="bg-red-700 text-white
+              w-full hover:bg-red-300"&gt; Clickable + Contained + Red + full width! &lt;/Button&gt;
+            </code>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
